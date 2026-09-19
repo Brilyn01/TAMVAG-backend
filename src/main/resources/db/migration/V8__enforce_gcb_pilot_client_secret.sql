7bd97e22d7daa83819e4020630e3a18c570fa806
Dude-dev1 <stephenyiadomosei@gmail.com>
2026-09-19 03:41:19 +0000

    reverting workspace

--- src/main/resources/db/migration/V8__enforce_gcb_pilot_client_secret.sql
+++
@@ -1,6 +0,0 @@
--- Ensure the seeded GCB pilot application's client secret
--- matches the credential defined for the pilot environment.
-
-UPDATE application
-SET client_secret_hash = '$2a$10$qVArwVjcer47PjzxyqBEPupYHiPhd7lMFSTDO6Wt.f15549k2kL8O'
-WHERE client_id = 'app_gcb_pilot_2026';
